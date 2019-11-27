1b8a3640c62bb6d1e3a6e19722737b036d8b5d79
최대희 <cdhbs@knetbiz.dooray.com>
2019-11-27 10:36:06 +0900

    2019.11.27 글쓰기 페이지 꾸미기

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Blog
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 # 장고에서 기본적으로 지원하는 forms
 # 블로그를 생성할 것
@@ -10,3 +11,14 @@ class CreateBlog(forms.ModelForm):
         model = Blog
         # pub_date는 자동입력되게 하여 삭제함
         fields = ['title', 'author', 'body']
+
+        widgets = {
+            'title' : forms.TextInput (
+                # attrs = 속성, 폼에 적용되는 css의 class 지정
+                attrs={'class': 'form-control', 'style': 'width: 100%', 'placeholder': '제목을 입력하세요.'}
+            ),
+            'author': forms.Select (
+                attrs={'class': 'custom-select'},
+            ),
+            'body': forms.CharField(widget=CKEditorUploadingWidget()),
+        }
